44d4739d4ba295d99d8f324a1a75edd18092e39c
ElanYoung <991658923@qq.com>
2022-10-21 19:53:36 +0800

    refactor: routes are divided by modules

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,11 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import HomeView from '@/views/HomeView.vue';
+
+import demoRouters from './modules/demo';
 
 Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView,
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (About.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import('../views/AboutView.vue'),
-  },
-];
+const routes = [...demoRouters];
 
 const router = new VueRouter({
   mode: 'history',
---
+++ src/router/modules/demo.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    path: '/',
+    name: 'home',
+    component: () => import('@/views/HomeView.vue'),
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import('@/views/AboutView.vue'),
+  },
+];
