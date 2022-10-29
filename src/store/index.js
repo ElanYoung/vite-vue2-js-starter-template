39dce4de30284fdf7cb33c9055056852f5cc7602
ElanYoung <991658923@qq.com>
2022-10-29 12:56:07 +0800

    refactor: use import.meta.glob method to bulk import modules

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,22 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
-import counter from './modules/counter';
+const files = import.meta.glob('./modules/*.js', {
+  eager: true,
+});
+
+const modules = [];
+Object.keys(files).forEach((key) => {
+  const moduleName = key.substring(key.lastIndexOf('/') + 1, key.lastIndexOf('.js'));
+  modules[moduleName] = files[key].default;
+});
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
   // 严格模式
   strict: import.meta.env.MODE !== 'production',
-  modules: { counter },
+  modules,
 });
 
 export default store;
